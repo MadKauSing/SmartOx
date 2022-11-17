2ec7433e0eb6f6aa062cc902837bdbc3040b86fd
CoderCatA5 <theayushsingh72@gmail.com>
2022-11-17 10:40:42 +0530

    deleted

---
+++ streamlit/streamlit-app.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+
+
+st.set_page_config(page_title="sMartOx")
+
+
+def smartox():
+    st.markdown('# sMartOx')
+    st.markdown('---')
+
+
